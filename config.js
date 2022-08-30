e677b467115d94a195ba6ae3cdb91db14d0e1dfe
bangokun <108464869+bangokun@users.noreply.github.com>
2022-08-30 09:12:35 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,21 +1,21 @@
 //=============『 Utama 』================== //
-global.owner = ['6287892711054'] 
+global.owner = ['6285738639969','6287789210430','6287892711054'] 
 global.mods = [] 
 global.prems = [] 
 
 //=============『 Info Owner 』============== //
-global.nameowner = 'Hyzer Official'
-global.numberowner = '6287892711054'
-global.instagram = 'https://instagram.com/hyzer_stfu'
-global.github = 'https://github.com/Hyzerr'
-global.dana = '087892711054'
-global.pulsa = '085212023766'
-global.gopay = '087892711054'
+global.nameowner = 'Chonix'
+global.numberowner = '84585961987'
+global.instagram = 'https://instagram.com/ktgakuma?igshid=YmMyMTA2M2Y='
+global.github = 'https://github.com/cheinope'
+global.dana = '085738639969'
+global.pulsa = '085738639969'
+global.gopay = '-'
 
 //=============『 Info Bot 』=================//
-global.namebot = 'Yuno Gasai Bot'
-global.gc = 'bit.ly/AnyaBot-Group'
-global.web = 'https://github.com/Hyzerr' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
+global.namebot = '𝐂𝐡𝐨𝐧𝐢𝐱-𝐁𝐎𝐓'
+global.gc = 'https://chat.whatsapp.com/JZcR7E5hOlE7dYbPiQhUtU'
+global.web = 'tiktok.com/@chonixb' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
 global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
 
 //=======『 Tampilan Dan Lainnya 』============//
@@ -23,21 +23,21 @@ global.fotonya1 = 'https://telegra.ph/file/e4a2f4339da8a32ad20a1.jpg' //ganti ja
 global.fotonya2 = 'https://telegra.ph/file/8c471d45b6cedad43cf84.jpg' //ini juga ganti 
 global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
-global.wm = 'ꜱʜɪʀᴏ ɪᴋᴀꜱᴀᴋɪ ʙᴏᴛ'
+global.wm = '𝐂𝐡𝐨𝐧𝐢𝐱-𝐁𝐎𝐓'
 global.watermark = wm
-global.wm2 = '                     「 𝚂𝙷𝙸𝚁𝙾-𝙼𝙳 あ⁩ 」'
-global.wm3 = '𝚂𝙷𝙸𝚁𝙾-𝙼𝙳'
+global.wm2 = '                     「 𝐂𝐡𝐨𝐧𝐢𝐱-𝐁𝐎𝐓 」'
+global.wm3 = 'Chonix'
 global.htki = '––––––『' 
 global.htka = '』––––––'
 global.media = 'https://telegra.ph/file/72168c6f1aaa90315c369.jpg'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.wait = '_*tunggu sedang di proses...*_'
+global.wait = '```「▰▰▰▱▱▱▱▱▱▱」Loading...```'
 global.eror = '_*Server Error*_'
 global.benar = 'Benar ✅\n'
 global.salah = 'Salah ❌\n'
 global.stiker_wait = 'Stiker sedang dibuat'
 global.packname = '2022'
-global.author = '© Hyzer Official'
+global.author = '𝐂𝐡𝐨𝐧𝐢𝐱-𝐁𝐎𝐓 ⫹⫺'
 
 //=============『 Apikey 』================== //
 global.APIs = { // API Prefix
